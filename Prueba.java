645721515220e274926ec5cd3951393e80e4d483
a19Ritavc <a19Ritavc@iessanclemente.net>
2020-04-19 18:00:54 +0200

    Segunda version, 2 metodos

--- Prueba.java
+++ Prueba.java
@@ -5,4 +5,41 @@ public class Prueba{
 		System.out.println(num*2);
 		return num*2;
 	}
+
+  public String queHacerHoy(String dia) {
+
+  switch(dia){
+
+  case "Lunes":
+    System.out.println("programacion");
+    break;
+
+  case "Martes":
+    System.out.println("sistemas infórmaticos");
+    break;
+
+  case "Miercoles":
+    System.out.println("bases de Datos");
+    break;
+
+  case "Jueves":
+    System.out.println("contornos");
+    break;
+
+  case "Viernes":
+    System.out.println("linguaxe de marcas");
+    break;
+
+  case "Sabado":
+    System.out.println("FOL");
+    break;
+
+  case "Domingo":
+    System.out.println("descanso");
+
+
+  }
+  return "";
+
+  }
 }
